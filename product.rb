65f01407ded851c2ad84039b8c7bf6594b7f2172
quantumduck <quantum.duck@hotmail.com>
2017-03-30 19:31:42 -0400

    more product methods for inventory

--- product.rb
+++ product.rb
@@ -5,6 +5,24 @@ class Product
   @@import_tax = 25
   @@serial_count = 0
 
+  def self.find(value, field = :name, cmp = :equal)
+    results = []
+    @@inventory.each do |product|
+      case field
+      when :name
+        results << product if product.name == value
+      when :price
+        case cmp
+        when :equal
+          results << product if product.price == value
+        when :lessthan
+          results << product if product.price < value
+        end
+      end
+    end
+    results
+  end
+
   attr_reader name
   attr_reader price
   attr_reader tax_rate
