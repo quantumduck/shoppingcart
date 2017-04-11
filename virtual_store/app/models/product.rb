465c3b5471bc2e217fa07e2e06f5e32e423a6346
quantumduck <quantum.duck@hotmail.com>
2017-04-11 19:36:29 -0400

    adding other fields to form and cleaning it up

--- virtual_store/app/models/product.rb
+++ virtual_store/app/models/product.rb
@@ -6,4 +6,14 @@ class Product < ApplicationRecord
     ((price * (100 + tax)).to_f / 100.0).round
   end
 
+  def formatted_price
+    extra_zero = ((price % 100) <  10)? "0" : ""
+    "$" + (price / 100).to_s + "." + extra_zero + (price % 100).to_s
+  end
+
+  def formatted_total
+    extra_zero = ((total % 100) <  10)? "0" : ""
+    "$" + (total / 100).to_s + "." + extra_zero + (total % 100).to_s
+  end
+
 end
